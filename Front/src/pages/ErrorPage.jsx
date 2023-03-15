5be3c6c1c7951d38903755aad3a671c2316195a9
htaylor91 <htaylor91@gmail.com>
2023-03-15 21:58:26 +0100

    feat(ErrorPage): ✨ add ErrorPage component

---
+++ Front/src/pages/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+function ErrorPage(){
+    return (
+        <div style={{display: 'grid', placeContent: 'center', height: '100vh'}}>
+            <h1>404</h1>
+            <p>Page not found</p>
+        </div>
+    )
+}
+
+export default ErrorPage
