36f2ec64a3cac30b5d9e98466ad24cabc5c52f56
htaylor91 <htaylor91@gmail.com>
2023-03-22 14:18:38 +0100

    refactor(ServiceContext): ♻️ update ServiceContext to manage its own state and export ServiceProvider

--- Front/src/contexts/ServiceContext.jsx
+++ Front/src/contexts/ServiceContext.jsx
@@ -1,3 +1,32 @@
-import { createContext} from "react";
+import { createContext, useState, useContext} from "react";
+import { SportSeeAPIMocked } from "../mocks/services/MockedService";
+import { SportSeeAPI } from "../services/APIService";
 
-export const ServiceContext = createContext();
+const ServiceContext = createContext();
+const ServiceUpdateContext = createContext();
+
+export function useService() {
+    return useContext(ServiceContext);
+}
+
+export function useServiceUpdate() {
+    return useContext(ServiceUpdateContext);
+}
+
+export function ServiceProvider({ children }) {
+    const [currentService, setCurrentService] = useState(SportSeeAPIMocked);
+
+    function toggleService() {
+        return currentService === SportSeeAPIMocked
+        ? setCurrentService(SportSeeAPI) 
+        : setCurrentService(SportSeeAPIMocked)
+    }
+
+    return (
+        <ServiceContext.Provider value={currentService}>
+            <ServiceUpdateContext.Provider value={toggleService}>
+                {children}
+            </ServiceUpdateContext.Provider>
+        </ServiceContext.Provider>
+    );
+}
