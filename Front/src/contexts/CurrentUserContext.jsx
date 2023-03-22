e9e71dd7b2ee372c27a0a6ce378c0831e67d107d
htaylor91 <htaylor91@gmail.com>
2023-03-22 14:17:48 +0100

    refactor(CurrentUserContext): ♻️ update CurrentUserContext to manage its own state and export CurrentUserProvider

--- Front/src/contexts/CurrentUserContext.jsx
+++ Front/src/contexts/CurrentUserContext.jsx
@@ -1,3 +1,28 @@
-import { createContext} from "react";
+import { createContext, useState, useContext} from "react";
 
-export const CurrentUserContext = createContext();
+const CurrentUserContext = createContext();
+const CurrentUserUpdateContext = createContext();
+
+export function useCurrentUser() {
+    return useContext(CurrentUserContext);
+}
+
+export function useCurrentUserUpdate() {
+    return useContext(CurrentUserUpdateContext);
+}
+
+export function CurrentUserProvider({ children }) {
+    const [currentUser, setCurrentUser] = useState(12);
+
+    function toggleCurrentUser() {
+        return currentUser === 12 ? setCurrentUser(18) : setCurrentUser(12)
+    }
+
+    return (
+        <CurrentUserContext.Provider value={currentUser}>
+            <CurrentUserUpdateContext.Provider value={toggleCurrentUser}>
+                {children}
+            </CurrentUserUpdateContext.Provider>
+        </CurrentUserContext.Provider>
+    );
+}
