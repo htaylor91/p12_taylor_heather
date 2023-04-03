477e6df296882658aa79e172f4b874cd2343331c
htaylor91 <htaylor91@gmail.com>
2023-04-03 17:12:26 +0200

    feat(Profile): ✨ import chart components into Profile page component, update classNames and structure

--- Front/src/pages/Profile.jsx
+++ Front/src/pages/Profile.jsx
@@ -1,36 +1,55 @@
+import '../styles/Profile.css'
 import useAxios from '../hooks/useAxios';
 import UserModel from '../models/UserModel.js';
+
 import Banner from '../components/Banner'
 import MacrosCard from '../components/MacrosCard';
+import Loading from '../components/Loading';
+import Error from '../components/Error';
+import Score from '../components/Score/Score';
+import Sessions from '../components/Sessions/Sessions';
+import Activity from '../components/Activity/Activity';
+import Performance from '../components/Performance';
 
+/**
+ * @component
+ */
 function Profile(){
-    const [data, loading, error] = useAxios('userService')
-
-    if(loading) return <div>Loading user data...</div>
-    if(error) return <div>ERROR loading user data</div>
+    const {loading, error, data} = useAxios('userService');
+    if(loading){
+        return (<Loading></Loading>)
+    }
+    if(error){
+        return (<Error></Error>)
+    }
     if(data){
         const user = new UserModel(data);
-        const firstName = user.firstName;
-        const keyData = user.keyData;
-        const score = user.score;
+        const {firstName, keyData, score} = user;
+
         return (
-            <>        
-                <main style={{display: 'flex', gap: '30px'}}>
-                    <article className="Profile">
+                <main className='container-horizontal Profile'>
+                    <article className='container-vertical'>
                         <Banner name={firstName}></Banner>
-                    </article>
-                    <aside>
-                        <div className="container">
-                            <MacrosCard macro='Calories' value={keyData?.calories}></MacrosCard>
-                            <MacrosCard macro='Glucides' value={keyData?.carbohydrates}></MacrosCard>
-                            <MacrosCard macro='Lipides' value={keyData?.lipids}></MacrosCard>
-                            <MacrosCard macro='Proteines' value={keyData?.proteins}></MacrosCard>
+                        <div className='container-horizontal Profile-charts'>
+                            <div className='container-vertical'>
+                                <Activity></Activity>
+                                <div className='container-horizontal Profile-square-charts'>
+                                    <Sessions></Sessions>
+                                    <Performance></Performance>
+                                    <Score score={score}></Score>
+                                </div>
+                            </div>
+                            <div className="container-vertical macros">
+                                <MacrosCard macro='Calories' value={keyData?.calories}></MacrosCard>
+                                <MacrosCard macro='Glucides' value={keyData?.carbohydrates}></MacrosCard>
+                                <MacrosCard macro='Lipides' value={keyData?.lipids}></MacrosCard>
+                                <MacrosCard macro='Proteines' value={keyData?.proteins}></MacrosCard>
+                            </div>
                         </div>
-                    </aside>
-                </main>
-            
-            </>
-    );}
+                    </article>
+                </main> 
+        );
+    }
 }
 
 export default Profile;
