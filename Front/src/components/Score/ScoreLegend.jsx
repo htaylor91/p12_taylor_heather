a6fc9bdff63615922147451b9d694babddd43b05
htaylor91 <htaylor91@gmail.com>
2023-04-03 16:05:32 +0200

    refactor(ScoreLegend): ♻️ delete ScoreLegend.css, add style object to add inline styles to ScoreLegend component

--- Front/src/components/Score/ScoreLegend.jsx
+++ Front/src/components/Score/ScoreLegend.jsx
@@ -1,16 +1,40 @@
-import './ScoreLegend.css'
 import PropTypes from 'prop-types';
 
+const ScoreLegendStyles = {
+    container: {
+        maxWidth: '95px',
+        margin: 'auto',
+    },
+    score: {
+        fontSize: '1.625rem',
+        fontWeight: '700',
+        color: 'var(--color-black)',
+        textAlign: 'center',
+    },
+    description: {
+        fontSize: '1rem',
+        fontWeight: '600',
+        color: 'var(--color-gray)',
+        textAlign: 'center',
+    }
+}
+
+/**
+ * @component
+ */
 function ScoreLegend({score}) {
     return (
-        <div className="ScoreLegend" style={{maxWidth: '95px', margin: 'auto'}}>
-            <p className='ScoreLegend-score'>{score}%</p>
-            <p className='ScoreLegend-description'>de votre objectif</p>
+        <div style={ScoreLegendStyles.container}>
+            <p style={ScoreLegendStyles.score}>{score}%</p>
+            <p style={ScoreLegendStyles.description}>de votre objectif</p>
         </div>
     );
 };
 
 ScoreLegend.propTypes = {
+    /**
+     * The score to display in the legend.
+     */
     score: PropTypes.number.isRequired
 };
 
