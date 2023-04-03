52537c5df214415179328ac57e15a020b69d0354
htaylor91 <htaylor91@gmail.com>
2023-04-03 15:28:49 +0200

    refactor(hooks): ♻️ update the custom useAxios hook to use the useCurrentUser and useCurrentService hooks
    add jsdoc comments, remove userId from the useEffect dependency array

--- Front/src/hooks/useAxios.jsx
+++ Front/src/hooks/useAxios.jsx
@@ -1,18 +1,25 @@
 import { useState, useEffect } from 'react';
-import { useCurrentUser } from '../contexts/CurrentUserContext';
-import { useService } from '../contexts/ServiceContext';
+import { useCurrentUser } from '../hooks/useCurrentUser';
+import { useCurrentService } from '../hooks/useCurrentService';
 
-export default function useAxios(subService) {
-    const userId = useCurrentUser();
-    const serviceContext = useService();
+/**
+ * A custom React hook to call the getData method of the current service
+ * @param {String} subService "activityService, userService, performanceService, sessionsService"
+ * @returns {{loading: boolean, error: boolean, data: Object | null}} - An object containing 3 properties: loading, error, and data
+ */
+function useAxios(subService) {
+    const {currentService} = useCurrentService();
+    const {currentUser} = useCurrentUser();
 
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
 
     useEffect(() => {
-        serviceContext[subService].getData(userId, setData, setLoading, setError);
-    }, [userId]);
+        currentService[subService].getData(currentUser, setData, setLoading, setError);
+    }, []);
 
-    return [loading, error, data];
+    return {loading, error, data};
 }
+
+export default useAxios;
