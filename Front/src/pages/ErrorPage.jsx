0eb6a8097fe3110d8ce868c5c7a2607b46425bd8
htaylor91 <htaylor91@gmail.com>
2023-04-07 11:39:00 +0200

    feat(ErrorPage): ✨ add 'return to homepage' Navlink to ErrorPage, add className, import ErrorPage.css file

--- Front/src/pages/ErrorPage.jsx
+++ Front/src/pages/ErrorPage.jsx
@@ -1,12 +1,18 @@
+import {NavLink} from 'react-router-dom'
+import '../styles/ErrorPage.css'
 /**
  * @component
  * Page to render when the user tries to access a page that doesn't exist
  */
 function ErrorPage(){
     return (
-        <div style={{display: 'grid', placeContent: 'center', height: '100vh'}}>
-            <h1>404</h1>
-            <p>Page not found</p>
+        <div className="ErrorPage">
+            <h1>Erreur 404</h1>
+            <p>Page non trouvée</p>
+            <NavLink 
+                className="navlink-return"
+                to="/">Retour à la page d'accueil
+            </NavLink>
         </div>
     )
 }
